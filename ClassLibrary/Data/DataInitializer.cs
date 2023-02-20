75bfea6b13c410ef8e2f7ccba3c6bcca02c9f93c
Richard Chalk <barmybrit@gmail.com>
2023-02-20 11:13:20 +0100

    Add project files.

---
+++ ClassLibrary/Data/DataInitializer.cs
@@ -0,0 +1,49 @@
+﻿using ClassLibrary.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Data
+{
+    public class DataInitializer
+    {
+        private readonly ICustomerService _customerService;
+        private readonly ApplicationDbContext _context;
+
+        public DataInitializer(ICustomerService customerService, ApplicationDbContext context)
+        {
+            _customerService = customerService;
+            _context = context;
+        }
+
+        public void SeedData()
+        {
+            _context.Database.Migrate();
+            SeedCountries();
+        }
+
+        private void SeedCountries()
+        {
+            CountryDoesNotExist("Sweden");
+            CountryDoesNotExist("Norway");
+            CountryDoesNotExist("Denmark");
+            CountryDoesNotExist("Finland");
+        }
+
+        private void CountryDoesNotExist(string countryLabel)
+        {
+            // Check to see if country already exists in db
+            if (_context.Countries.Any(c => c.CountryLabel == countryLabel)) return;
+            
+            _context.Countries
+                .Add(new Country
+                {
+                    CountryLabel = countryLabel
+                });
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ MVCBasicCrudStart/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using ClassLibrary.Data;
+using ClassLibrary.DTOs;
+using ClassLibrary.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Tenta.Models.Customer;
+
+namespace Tenta.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _customerService;
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ICustomerService customerService, ApplicationDbContext context)
+        {
+            _customerService = customerService;
+            _context = context;
+        }
+
+        // READ ALL -  READ ALL - READ ALL - READ ALL - READ ALL - READ ALL - READ ALL -
+        // READ ALL -  READ ALL - READ ALL - READ ALL - READ ALL - READ ALL - READ ALL -
+        // READ ALL -  READ ALL - READ ALL - READ ALL - READ ALL - READ ALL - READ ALL -
+        // READ ALL -  READ ALL - READ ALL - READ ALL - READ ALL - READ ALL - READ ALL -
+        public IActionResult Customers(string q)
+        
+        {
+            var customersVM = new CustomersVM();
+            customersVM.Customers = _customerService.GetAllCustomers(q);
+            customersVM.Countries = _customerService.FillCountryDropDown();
+
+            return View(customersVM);
+        }
+
+        // CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - 
+        // CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - 
+        // CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - 
+        // CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - CREATE - 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Customers(CustomersVM customersVM)
+        {
+            if (ModelState.IsValid)
+            {
+                _customerService.CreateCustomer(customersVM.CustomerCreateDTO);
+
+                // Used for Toastr notifications
+                TempData["success"] = "Customer created successfully";
+
+                _context.SaveChanges();
+                return RedirectToAction("Customers", "Customer");
+            }
+
+            customersVM.Customers = _customerService.GetAllCustomers(customersVM.q);
+            customersVM.Countries = _customerService.FillCountryDropDown();
+
+            return View(customersVM);
+        }
+    }
+}
